3f58bea797bf033cd5cf8d1df2a7afbde536b0e5
ACAM023 <sonalpha023@gmail.com>
2020-10-03 19:08:25 +0300

    Initial commit

---
+++ queries.js
@@ -0,0 +1,35 @@
+let promise = require('bluebird');
+
+let options = {
+    // Initialization Options
+    promiseLib: promise
+};
+
+let pgp = require('pg-promise')(options);
+let connectionString = 'postgres://postgres@localhost:5432/puppies';
+let db = pgp(connectionString);
+
+// add query functions
+
+module.exports = {
+    getAllPuppies: getAllPuppies,
+    // getSinglePuppy: getSinglePuppy,
+    // createPuppy: createPuppy,
+    // updatePuppy: updatePuppy,
+    // removePuppy: removePuppy
+};
+
+function getAllPuppies(req, res, next) {
+    db.any('SELECT * FROM pups')
+      .then(function (data) {
+          res.status(200)
+            .json({
+                status: 'success',
+                data: data,
+                message: 'Retrieved All puppies'
+            });
+      })
+      .catch(function (error) {
+          return next(error);
+      });
+}
